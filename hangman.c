3ccf872a1da58d78ede8329edafcc26b9416f324
Eric <eric.anacleto@gmail.com>
2023-10-13 19:32:52 +0200

    Working on the function to get user's input

--- hangman.c
+++ hangman.c
@@ -2,15 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_WORDS 100
 #define MAX_WORD_LENGTH 20
+
+char ask_for_input(int guess_number);
 
 int main(void)
 {
     // defining variables
     char *words[MAX_WORDS];
     int word_count = 0;
+    char guess;
 
     // opening the txt file with words to be ramdonly used in the game
     FILE *word_list = fopen("words.txt", "r");
@@ -27,7 +31,7 @@ int main(void)
         words[i] = (char *)malloc(MAX_WORD_LENGTH);
         if (words[i] == NULL)
         {
-            printf("Memory allocation failed.\n");
+            printf("Memory allocation failed for word %d.\n", i);
             exit(1);
         }
 
@@ -44,7 +48,8 @@ int main(void)
     int chosen_word_index = rand() % word_count;
 
     // saving the word in a new array and freeing memory
-    char selected_word[strlen(words[chosen_word_index]) + 1];
+    int word_length = strlen(words[chosen_word_index]);
+    char selected_word[word_length + 1];
     strcpy(selected_word, words[chosen_word_index]);
 
     for (int i = 0; i < word_count; i++)
@@ -53,6 +58,7 @@ int main(void)
         words[i] = NULL;
     }
 
+    // vitual presentation of the game
     printf("\n\n");
     printf("*********\n");
     printf(" HANGMAN\n");
@@ -60,4 +66,21 @@ int main(void)
     printf("\n\n");
 
     return 0;
+}
+
+char ask_for_input(int guess_number) // asking for user's input and validating it
+{
+    char input;
+    do
+    {
+        printf("Insert your %d guess: ", guess_number);
+        scanf(" %c", &input);
+        input = toupper(input);
+        if (input < 'A' || input > 'Z')
+        {
+            printf("Please, insert only characters from A to Z.\n");
+        }
+    } while (input < 'A' || input > 'Z');
+
+    return input;
 }
